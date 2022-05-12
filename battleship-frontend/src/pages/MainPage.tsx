44d852a30f7584fc2b08a1d75879df8f07eaf6f6
ZuzaTwardowska <51333262+ZuzaTwardowska@users.noreply.github.com>
2022-05-12 11:41:49 +0200

    init react project

---
+++ battleship-frontend/src/pages/MainPage.tsx
@@ -0,0 +1,8 @@
+import {Link} from "react-router-dom"
+function MainPage() {
+    return (
+      <Link to="/simulation"><button>Start simulation</button></Link>
+    );
+  }
+
+  export default MainPage;
---
+++ battleship-frontend/src/pages/SimulationPage.tsx
@@ -0,0 +1,8 @@
+
+function SimulationPage() {
+    return (
+      <h2>simulation</h2>
+    );
+  }
+
+  export default SimulationPage;
